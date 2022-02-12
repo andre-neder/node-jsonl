df3dc8481bd202dd5b7f5ffc9f6d9bc992a7ed81
André Neder <andre.neder@mail.de>
2022-02-12 19:13:07 +0100

    added @params

--- node-jsonl.js
+++ node-jsonl.js
@@ -1,9 +1,18 @@
+/*
+  To learn more about jsonl visit: https://jsonlines.org
+*/
 class JSONL{
-    /* Creates JSONL String from array of objects */
+    /**
+     * Create a jsonl formated string from an array of objects.
+     * @param {Object[]} array - The array of objects.
+     */
     static stringify(array){
       return array.map(j => JSON.stringify(j)).join('\n');
     }
-    /* Creates array of Objects from JSONL */
+    /**
+     * Parse a jsonl formated string to an array of objects.
+     * @param {string} jsonl - The string to parse.
+     */
     static parse(jsonl){
       return jsonl.split('\n').filter(j => j != '').map(j => JSON.parse(j))
     }
